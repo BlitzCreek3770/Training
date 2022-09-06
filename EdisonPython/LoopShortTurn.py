e7c6bc94327e2dc48e299a2b0ee180a5f45ed53f
teklingl <teklingl@gmail.com>
2022-09-06 13:13:20 -0400

    Latest version.

---
+++ EdisonPython/LoopShortTurn.py
@@ -0,0 +1,21 @@
+# This program demonstrates a for-loop by performing
+# a simple turn action a given number of times
+# FRC 3770 Edison Python Programming
+#-------------Setup----------------
+
+import Ed
+
+Ed.EdisonVersion = Ed.V2
+
+Ed.DistanceUnits = Ed.INCH
+Ed.Tempo = Ed.TEMPO_MEDIUM
+
+#--------Your code below-----------
+
+count = 5
+
+# Turn rignt and pivot back to left the given number
+# of times as dicated by the loop
+for n in range(count):
+    Ed.Drive(Ed.SPIN_RIGHT,Ed.SPEED_3,90)
+    Ed.Drive(Ed.FORWARD_LEFT,Ed.SPEED_3,90)
