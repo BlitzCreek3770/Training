ea8799079839a20e6e9e8a9f940c9d9cb1642724
teklingl <teklingl@gmail.com>
2022-10-10 15:25:04 -0400

    Latest version.

--- JavaBasics/BasicConsoleOutput.java
+++ JavaBasics/BasicConsoleOutput.java
@@ -1,13 +1,14 @@
-
-
 public class BasicConsoleOutput 
 {
    public static void main( String args[] )
    {
+      // Variable declarations
+      int    teamNum   = 3770;
+      double pi     = 3.1415926;
+      String school = "Delta";
 
-
-
-      System.out.println("BlitzCreek 3770");
-
+      // Various statements that write to the console
+      System.out.println("BlitzCreek");
+      System.out.println("Team: " + teamNum);
    }
 }
