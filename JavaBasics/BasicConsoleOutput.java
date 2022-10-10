e3c1c5216cdcc4d137aa13d7b0713643f8d58b9f
teklingl <teklingl@gmail.com>
2022-10-10 17:54:37 -0400

    Latest version.

--- JavaBasics/BasicConsoleOutput.java
+++ JavaBasics/BasicConsoleOutput.java
@@ -1,14 +1,21 @@
-public class BasicConsoleOutput 
+// Team 3770 Robotics Programming
+// Example: BasicConsoleOutput
+//
+
+public class BasicConsoleOutput
 {
    public static void main( String args[] )
    {
       // Variable declarations
       int    teamNum   = 3770;
-      double pi     = 3.1415926;
-      String school = "Delta";
+      double score     = 99.9;
+      String school    = "Delta";
 
       // Various statements that write to the console
+      System.out.println("Welcome to Java Programming");
+      System.out.println();
       System.out.println("BlitzCreek");
       System.out.println("Team: " + teamNum);
+      System.out.println("Score: " + score + " points");
    }
 }
