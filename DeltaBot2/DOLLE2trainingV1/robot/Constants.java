d24e0c42097d8e850f2b57840ff47348ba67d508
teklingl <teklingl@gmail.com>
2023-01-29 15:14:51 -0500

    Latest working code.

---
+++ DeltaBot2/DOLLE2trainingV1/robot/Constants.java
@@ -0,0 +1,37 @@
+// Delta College/Team 3770 Robotics Programming
+// Contains all constant declarations for port numbers and other
+// parameters necessary for robot operation and tuning.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+
+/* Vendor library URLs 
+ * CTRE:   https://maven.ctr-electronics.com/release/com/ctre/phoenix/Phoenix5-frc2023-latest.json
+ * 
+ * 
+*/
+
+public final class Constants 
+{
+    // Driver motors
+    public static final int LEFT_MOTOR1_CAN_ID   = 2;
+    public static final int LEFT_MOTOR2_CAN_ID   = 3;
+    public static final int RIGHT_MOTOR1_CAN_ID  = 4;
+    public static final int RIGHT_MOTOR2_CAN_ID  = 1;
+
+    // Joystick control
+    public static final int LEFT_STICK_USB_PORT  = 0;
+    public static final int RIGHT_STICK_USB_PORT = 1;
+
+    // Auton limits
+    public static final double SIMPLE_AUTON_DRIVE_TIME    = 3.0;
+    public static final double SIMPLE_AUTON_POWER         = 0.5;
+}
